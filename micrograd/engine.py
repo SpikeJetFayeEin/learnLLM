ead6a6d96b3b28dce5e173c6112b876ef6fa144e
spike <a873444208@163.com>
2026-01-02 11:43:50 +0800

    feat nn.py

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,11 +1,13 @@
 class Value:
+    """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, children=(), op=''):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-        self.backward = lambda: None
-        self.prev = set(children)
-        self.op = op
+        # internal variables used for autograd graph construction
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op  # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -15,60 +17,62 @@ class Value:
             self.grad += out.grad
             other.grad += out.grad
 
-        out.backward = _backward
-        return out
+        out._backward = _backward
 
-    def __sub__(self, other):  # self - other
-        return self + (-other)
+        return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad += out.grad * other.data
-            other.grad += out.grad * self.data
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
 
-        out.backward = _backward
+        out._backward = _backward
+
         return out
-
-    def __truediv__(self, other):  # self / other
-        return self * other ** -1
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data ** other, (self, other), '**')
+        out = Value(self.data ** other, (self,), f'**{other}')
 
         def _backward():
-            self.grad += out.grad * other * self.data ** (other - 1)
+            self.grad += (other * self.data ** (other - 1)) * out.grad
 
-        out.backward = _backward
+        out._backward = _backward
+
         return out
 
     def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'Relu')
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
 
         def _backward():
-            self.grad += out.grad * (self.data > 0)
+            self.grad += (out.data > 0) * out.grad
 
-        out.backward = _backward
+        out._backward = _backward
+
         return out
 
     def backward(self):
+
+        # topological order all of the children in the graph
         topo = []
         visited = set()
 
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
-                for child in v.prev:
+                for child in v._prev:
                     build_topo(child)
                 topo.append(v)
 
         build_topo(self)
 
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
         for v in reversed(topo):
-            v.backward()
+            v._backward()
 
     def __neg__(self):  # -self
         return self * -1
@@ -76,11 +80,17 @@ class Value:
     def __radd__(self, other):  # other + self
         return self + other
 
+    def __sub__(self, other):  # self - other
+        return self + (-other)
+
     def __rsub__(self, other):  # other - self
         return other + (-self)
 
     def __rmul__(self, other):  # other * self
         return self * other
+
+    def __truediv__(self, other):  # self / other
+        return self * other ** -1
 
     def __rtruediv__(self, other):  # other / self
         return other * self ** -1
